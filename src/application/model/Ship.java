ecf283d58f5d01f997d41848d394665252b33cbe
wolfyCSA <bhawk11@gmail.com>
2018-04-10 02:17:08 -0500

    Updated superclass variables and methods

--- src/application/model/Ship.java
+++ src/application/model/Ship.java
@@ -8,11 +8,77 @@
 
 package application.model;
 
+import java.util.ArrayList;
+
 public class Ship {
 	
-	private int 	health;
-	private int 	speed;
-	private String weaponID;
-	private String spriteLink;
+	protected int    			hullPoints;
+	protected int	 			shieldPoints;
+	protected String 			spriteLink;
+	protected int 				numWeapons;
+	protected ArrayList<String> weapons;
+	
+	/**
+	 * @return the hullPoints
+	 */
+	public int getHullPoints() {
+		return hullPoints;
+	}
+	/**
+	 * @return the shieldPoints
+	 */
+	public int getShieldPoints() {
+		return shieldPoints;
+	}
+	/**
+	 * @return the spriteLink
+	 */
+	public String getSpriteLink() {
+		return spriteLink;
+	}
+	/**
+	 * @return the numWeapons
+	 */
+	public int getNumWeapons() {
+		return numWeapons;
+	}
+	/**
+	 * @return the weapons
+	 */
+	public ArrayList<String> getWeapons() {
+		return weapons;
+	}
+	/**
+	 * @param hullPoints the hullPoints to set
+	 */
+	public void setHullPoints(String hullPoints) {
+		this.hullPoints = Integer.parseInt(hullPoints);
+	}
+	/**
+	 * @param shieldPoints the shieldPoints to set
+	 */
+	public void setShieldPoints(String shieldPoints) {
+		this.shieldPoints = Integer.parseInt(shieldPoints);
+	}
+	/**
+	 * @param spriteLink the spriteLink to set
+	 */
+	public void setSpriteLink(String spriteLink) {
+		this.spriteLink = spriteLink;
+	}
+	/**
+	 * @param numWeapons the numWeapons to set
+	 */
+	public void setNumWeapons(String numWeapons) {
+		this.numWeapons = Integer.parseInt(numWeapons);
+	}
+	/**
+	 * @param weapons the weapons to set
+	 */
+	public void addWeapons(String weaponID) {
+		weapons.add(weaponID);
+	}
+	
+	
 
-}
+}//end class Ship
