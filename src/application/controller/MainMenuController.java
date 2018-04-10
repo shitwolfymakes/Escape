adbcfdbfe957829160eb343bf5472955f932ba23
caseycannon423 <c.cannon29@gmail.com>
2018-04-09 20:16:32 -0500

    Created Main Menu on launch
    Buttons "work"

--- src/application/controller/MainMenuController.java
+++ src/application/controller/MainMenuController.java
@@ -6,6 +6,38 @@
 
 package application.controller;
 
-public class MainMenuController {
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainMenuController implements EventHandler<Event>{
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private Button loadButton;
+
+    @FXML
+    private Button highScoreButton;
+
+    @FXML
+    public void start(Event event) {
+    	System.out.print("butt");
+    }
+
+    @Override
+	public void handle(Event event) {
+    	System.out.print("dink");
+    }
+
+    @FXML
+    public void highScore(Event event) {
+    	System.out.print("stink");
+    }
+
+
 
 }
