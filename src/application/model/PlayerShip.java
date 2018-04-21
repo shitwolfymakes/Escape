7a2538f93054ff155b67468c122e53cea30627f8
wolfyCSA <bhawk11@gmail.com>
2018-04-20 20:43:44 -0500

    Added constructor, setters, and getters

--- src/application/model/PlayerShip.java
+++ src/application/model/PlayerShip.java
@@ -6,6 +6,46 @@
 
 package application.model;
 
-public class PlayerShip extends Ship {
+import java.util.ArrayList;
 
-}
+import application.Main;
+
+public class PlayerShip extends Ship {
+	
+	private int currentRow;
+	private int currentCol;
+	
+	public PlayerShip(int r, int c) {
+		this.currentRow = r;
+		this.currentCol = c;
+		
+		//super.setHullPoints(Main.profile.getCurrentHull())
+		//super.setShieldPoints(Main.profile.getCurrentShield()); // Not using this in this version
+		//super.setNumWeapons(Main.profile.getNumWeapons());
+		super.setSpriteLink("data/Escape Sprites/playership.png");
+		//super.addWeapons(Main.cortex.getWeapon("W_"+ Main.profile.getCurrentWeaponID()));
+		
+		//for (int i = 0; i < getNumWeapons(); i++)
+		System.out.println(this.toString());
+	}//end PlayerShip()
+
+	public void setCurrentLocation(int row, int column) {
+		this.currentRow = row;
+		this.currentCol = column;
+	}
+
+	/**
+	 * @return the currentRow
+	 */
+	public int getCurrentRow() {
+		return currentRow;
+	}
+
+	/**
+	 * @return the currentCol
+	 */
+	public int getCurrentCol() {
+		return currentCol;
+	}
+	
+}//end class PlayerShip
