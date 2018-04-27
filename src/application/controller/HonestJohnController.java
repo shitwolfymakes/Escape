5a0832466a157e153b783070ab6bc0958df3cd29
Michael Indo <michael.indo12@gmail.com>
2018-04-27 17:55:22 -0500

    updated honest johns

--- src/application/controller/HonestJohnController.java
+++ src/application/controller/HonestJohnController.java
@@ -6,6 +6,47 @@
 
 package application.controller;
 
-public class HonestJohnController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class HonestJohnController implements EventHandler<Event>, Initializable{
+	
+    int cashAmount = 3500;
+	String cashString = "$";
+	@FXML
+	Button buyBeer;
+	
+	@FXML
+	Text money;
+
+	@Override
+	public void handle(Event event) {
+		if(((Button)event.getSource()).equals(buyBeer)) {
+			cashAmount -= 3;
+			cashString += String.valueOf(cashAmount);
+		    money.setText(cashString);
+		    cashString = "$";
+	}
+		
+	
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		cashString += String.valueOf(cashAmount);
+	    money.setText(cashString);
+	    cashString = "$";
+		
+	}
+	
 
 }
